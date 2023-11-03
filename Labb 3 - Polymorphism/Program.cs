ea0e92d0e691c1637dc46891500c466b74a97879
Bighanz98 <Antonhanz9@gmail.com>
2023-11-03 02:10:58 +0100

    Yo

--- Labb 3 - Polymorphism/Program.cs
+++ Labb 3 - Polymorphism/Program.cs
@@ -3,27 +3,29 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Anton Hansson SUT23
+
         Geometry geometry = new Geometry("Color"); //Skapar objekt för alla klasser.
 
-        Rectangle rectangle = new Rectangle("Grön",6, 4);
-        Console.ForegroundColor = ConsoleColor.Green;
+        Rectangle rectangle = new Rectangle("Grön",6, 4); //Väljer 6 cm som bas och 4 cm som höjd.
+        Console.ForegroundColor = ConsoleColor.Green; //Ändrar färg.
         Console.WriteLine("Rektangel");
         Console.ResetColor();
-        Console.WriteLine("Area: " + rectangle.Area() + " cm , Färg : " + rectangle._color);
+        Console.WriteLine("Area: " + rectangle.Area() + " cm , Färg : " + rectangle._color); //Kallar på metoden för att skriva ut uträkningen.
 
 
-        Square square = new Square("Blå", 8);
+        Square square = new Square("Blå", 8); //Kvadratens sida är 8 cm.
         Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine("\nKvadrat");
         Console.ResetColor();
         Console.WriteLine("Area: " + square.Area() + " cm , Färg : " + square._color);
 
 
-        Circle circle = new Circle("Red",3.14, 10);
+        Circle circle = new Circle("Röd",3.14, 10); //Pi och radien 10 cm.
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("\nCirkel");
         Console.ResetColor();
-        Console.WriteLine("Area: " + circle.Area() + " cm , Färg : " + circle._color);
+        Console.WriteLine("Area: " + circle.Area() + " cm2 , Färg : " + circle._color);
 
 
         Console.ReadKey();
